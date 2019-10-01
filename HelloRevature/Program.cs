edaddb9f22209ebeedd09453e0251244765154d2
Joe Mohrbacher <jhmohrbacher@gmail.com>
2019-10-01 11:36:49 -0500

    add gitignor for vs, vs code.

---
+++ HelloRevature/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloRevature
+{
+    // some change for git to track
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Revature!");
+        }
+    }
+}
